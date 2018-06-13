1455c7cf730eebb51fdae8bdd40d01c1e56b7d04
pengrj <pengrj@emrubik.com>
2018-06-13 18:21:33 +0800

    微信环境搭建

--- test/java/testMybatis/testMybatis.java
+++ test/java/testMybatis/testMybatis.java
@@ -6,7 +6,6 @@ import com.wechat.service.CountryService;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
